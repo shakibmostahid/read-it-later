0966ff505719417c69e80126d569322b204d2f50
Shakib <shakib.mostahid@outlook.com>
2021-06-18 01:53:44 +0600

    add contents table migration.

---
+++ database/migrations/2021_06_17_194456_create_contents_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contents', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('pocket_id');
+            $table->string('url');
+            $table->timestamps();
+
+            $table->foreign('pocket_id')
+                ->references('id')
+                ->on('pockets')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contents');
+    }
+}
